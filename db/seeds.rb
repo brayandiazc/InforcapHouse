40e9775d93ab864d9c446cc8841ddb30a351d2a9
Brayan Diaz C <brayandiazc@gmail.com>
2023-10-07 09:11:54 -0500

    Seed/csv Tipo de inmueble

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+
+puts 'Importing typeProperties...'
+CSV.foreach(Rails.root.join('db/seeds/csv/typeProperties.csv'), headers: true) do |row|
+  TypeProperty.create! do |type_property|
+    type_property.id = row[0]
+    type_property.name = row[1]
+  end
+end
