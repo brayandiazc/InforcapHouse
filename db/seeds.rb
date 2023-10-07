513e0193b274867208ce1b69a059a8adbb91b561
Brayan Diaz C <brayandiazc@gmail.com>
2023-10-07 09:24:21 -0500

    Seed/csv Caracteristica

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,25 @@
 require 'csv'
 
-puts 'Importing typeProperties...'
-CSV.foreach(Rails.root.join('db/seeds/csv/typeProperties.csv'), headers: true) do |row|
-  TypeProperty.create! do |type_property|
-    type_property.id = row[0]
-    type_property.name = row[1]
-  end
-end
+# puts 'Importing typeProperties...'
+# CSV.foreach(Rails.root.join('db/seeds/csv/typeProperties.csv'), headers: true) do |row|
+#   TypeProperty.create! do |type_property|
+#     type_property.id = row[0]
+#     type_property.name = row[1]
+#   end
+# end
 
-puts 'Importing typeOffers...'
-CSV.foreach(Rails.root.join('db/seeds/csv/typeOffers.csv'), headers: true) do |row|
-  TypeOffer.create! do |type_offer|
-    type_offer.id = row[0]
-    type_offer.name = row[1]
+# puts 'Importing typeOffers...'
+# CSV.foreach(Rails.root.join('db/seeds/csv/typeOffers.csv'), headers: true) do |row|
+#   TypeOffer.create! do |type_offer|
+#     type_offer.id = row[0]
+#     type_offer.name = row[1]
+#   end
+# end
+
+puts 'Importing features...'
+CSV.foreach(Rails.root.join('db/seeds/csv/features.csv'), headers: true) do |row|
+  Feature.create! do |feature|
+    feature.id = row[0]
+    feature.name = row[1]
   end
 end
